cd6fe231db2d43c04ad8805256fe74f020315a7a
Thiago Lima <thiagotigaz@gmail.com>
2021-05-16 23:58:01 -0700

    Fixed migrations

--- SurfersLand/SurfersLand/Migrations/Configuration.cs
+++ SurfersLand/SurfersLand/Migrations/Configuration.cs
@@ -1,16 +1,23 @@
-﻿
-using System;
-using System.Data.Entity;
-using System.Data.Entity.Migrations;
-using System.Linq;
-
 namespace SurfersLand.Migrations
 {
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
     internal sealed class Configuration : DbMigrationsConfiguration<SurfersLand.Models.ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
-    } 
+
+        protected override void Seed(SurfersLand.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
 }
